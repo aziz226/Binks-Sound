d0cc48a057f8cabd0153c81b5c169c92dabefaa4
aziz226 <abdoulazizebagagnan@gmail.com>
2023-07-30 14:46:08 +0000

    init

---
+++ app/src/main/java/com/exopen/binkssound/Identifiant/SetUp.java
@@ -0,0 +1,16 @@
+package com.exopen.binkssound.Identifiant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exopen.binkssound.R;
+
+public class SetUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up);
+    }
+}
